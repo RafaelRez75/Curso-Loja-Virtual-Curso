611f76eb4460033ce6f298938dcdf039019ef9f5
RafaelRez75 <rrezende75@gmail.com>
2020-09-28 13:12:03 -0300

    Aula 16, Seção 4 - Navegando Via Provider

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/models/page_manager.dart';
 import 'package:lojavirtual/screens/common/custom_drawer/custom_drawer.dart';
+import 'package:provider/provider.dart';
 
 class BaseScreen extends StatelessWidget {
   final PageController pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      controller: pageController,
-      physics: const NeverScrollableScrollPhysics(),
-      children: <Widget>[
-        Scaffold(
-          drawer: CustomDrawer(),
-          appBar: AppBar(
-            title: const Text("nome"),
+    return Provider(
+      create: (_) => PageManager(pageController),
+      child: PageView(
+        controller: pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: <Widget>[
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text("nome"),
+            ),
           ),
-        ),
-        Container(
-          color: Colors.green,
-        ),
-        Container(
-          color: Colors.yellow,
-        ),
-      ],
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text("nome1"),
+            ),
+          ),
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text("nome2"),
+            ),
+          ),
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text("nome3"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
