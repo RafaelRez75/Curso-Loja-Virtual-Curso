c12e39a5200f247aab3f82b6504c6e0f9706257a
RafaelRez75 <rrezende75@gmail.com>
2020-10-09 13:29:32 -0300

    Aula 24, Seção 6 - Validando e Salvando os Dados

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,8 +1,12 @@
 class User{
 
-  User({this.email, this.password});
+  User({this.email, this.password, this.name});
 
+  String name;
   String email;
   String password;
+
+  String confirmPassword;
+
 
 }
