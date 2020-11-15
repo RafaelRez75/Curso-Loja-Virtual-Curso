e7c16610d9aab9c2b0589b780b9d7d276601ed8b
RafaelRez75 <rrezende75@gmail.com>
2020-11-15 00:15:30 -0300

    Aula 39, Seção 8 - Selecionando um Tamanho

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,7 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:lojavirtual/models/item_size.dart';
 
-class Product{
+class Product extends ChangeNotifier{
 
   Product.fromDocument(DocumentSnapshot document){
       id = document.documentID;
@@ -10,14 +11,21 @@ class Product{
       images = List<String>.from(document.data['images'] as List<dynamic>);
       sizes = (document.data['sizes'] as List<dynamic> ?? []).map(
               (s) => ItemSize.fromMap(s as Map<String, dynamic>)).toList();
-  print(sizes);
   }
 
   String id;
   String name;
   String description;
   List<String> images;
-
   List<ItemSize> sizes;
+
+  ItemSize _selectedSize;
+
+  ItemSize get selectedSize => _selectedSize;
+
+  set selectedSize(ItemSize value){
+    _selectedSize = value;
+    notifyListeners();
+  }
 
 }
--- lib/screens/detailproduct/product_screen.dart
+++ lib/screens/detailproduct/product_screen.dart
@@ -2,6 +2,7 @@ import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/models/product.dart';
 import 'package:lojavirtual/screens/detailproduct/components/size_widget.dart';
+import 'package:provider/provider.dart';
 
 class ProductScreen extends StatelessWidget {
 
@@ -13,97 +14,100 @@ class ProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final  primaryColor = Theme.of(context).primaryColor;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(product.name),
-        centerTitle: true,
-      ),
-      backgroundColor: Colors.white,
-      body: ListView(
-        children: <Widget>[
-            AspectRatio(
-              aspectRatio: 1.2,
-              child: Carousel(
-                images: product.images.map((url){
-                  return NetworkImage(url);
-                }).toList(),
-                dotSize: 4,
-                dotBgColor: Colors.transparent,
-                dotColor: primaryColor,
-                autoplay: false,
-                dotSpacing: 18,
+    return ChangeNotifierProvider.value(
+      value: product,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(product.name),
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.white,
+        body: ListView(
+          children: <Widget>[
+              AspectRatio(
+                aspectRatio: 1.2,
+                child: Carousel(
+                  images: product.images.map((url){
+                    return NetworkImage(url);
+                  }).toList(),
+                  dotSize: 4,
+                  dotBgColor: Colors.transparent,
+                  dotColor: primaryColor,
+                  autoplay: false,
+                  dotSpacing: 18,
+                ),
               ),
-            ),
 
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  product.name,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600
-                  ),
-                ),
-                Padding(
-                padding: const EdgeInsets.only(top: 8),
-                  child: Text(
-                    'A partir de',
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    product.name,
                     style: TextStyle(
-                      fontSize: 13,
-                      color: Colors.grey[600]
-                    ),
-                  )
-                ),
-                Text(
-                  'R\$ 19,99',
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    color: primaryColor
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top:16, bottom: 8),
-                  child: Text(
-                    'Descrição',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600
                     ),
                   ),
-                ),
-                Text(
-                  product.description,
-                  style: const TextStyle(
-                      fontSize: 16
+                  Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                    child: Text(
+                      'A partir de',
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: Colors.grey[600]
+                      ),
+                    )
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top:16, bottom: 8),
-                  child: Text(
-                    'Tamanhos',
+                  Text(
+                    'R\$ 19,99',
                     style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:16, bottom: 8),
+                    child: Text(
+                      'Descrição',
+                      style: TextStyle(
                         fontSize: 16,
                         fontWeight: FontWeight.w500
+                      ),
                     ),
                   ),
-                ),
-                Wrap(
-                  spacing: 8,
-                  runSpacing: 8,
-                  children: product.sizes.map((s){
-                    return SizeWidget(size: s);
-                  }).toList(),
+                  Text(
+                    product.description,
+                    style: const TextStyle(
+                        fontSize: 16
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top:16, bottom: 8),
+                    child: Text(
+                      'Tamanhos',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500
+                      ),
+                    ),
+                  ),
+                  Wrap(
+                    spacing: 8,
+                    runSpacing: 8,
+                    children: product.sizes.map((s){
+                      return SizeWidget(size: s);
+                    }).toList(),
 
-                )
-              ],
-            ),
-          )
-        ],
-      )
+                  )
+                ],
+              ),
+            )
+          ],
+        )
+      ),
     );
   }
 }
