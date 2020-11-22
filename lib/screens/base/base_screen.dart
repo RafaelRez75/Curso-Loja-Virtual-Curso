fd7deb76c5cbd87a35923232f31258725c3a9f53
RafaelRez75 <rrezende75@gmail.com>
2020-11-22 18:41:13 -0300

    Aula 54, Seção 10 - Criando a Barra Flutuante

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/models/page_manager.dart';
 import 'package:lojavirtual/screens/common/custom_drawer/custom_drawer.dart';
+import 'package:lojavirtual/screens/home/home_screen.dart';
 import 'package:lojavirtual/screens/products/products_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -15,12 +16,7 @@ class BaseScreen extends StatelessWidget {
         controller: pageController,
         physics: const NeverScrollableScrollPhysics(),
         children: <Widget>[
-          Scaffold(
-            drawer: CustomDrawer(),
-            appBar: AppBar(
-              title: const Text("nome1"),
-            ),
-          ),
+          HomeScreen(),
           ProductsScreen(),
           Scaffold(
             drawer: CustomDrawer(),
