44c265e47a549c4f4a8298b27a5d3fce4a4e3ecd
RafaelRez75 <rrezende75@gmail.com>
2020-11-24 15:49:42 -0300

    Aula 62, Seção 12 - Buscando os Usuários no Firebase

--- lib/models/admin_users_manager.dart
+++ lib/models/admin_users_manager.dart
@@ -1,4 +1,6 @@
-import 'package:faker/faker.dart';
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/models/user.dart';
 import 'package:lojavirtual/models/user_manager.dart';
@@ -7,27 +9,38 @@ class AdminUsersManager extends ChangeNotifier{
 
   List<User> users = [];
 
+  final Firestore firestore = Firestore.instance;
+
+  StreamSubscription _subscription;
+
   void updateUser(UserManager userManager){
+    _subscription?.cancel();
     if(userManager.adminEnabled){
       _listenToUsers();
+    }else{
+      users.clear();
+      notifyListeners();
     }
   }
 
   void _listenToUsers(){
-    const faker = Faker();
-    for(int i=0; i< 1000; i++){
-      users.add(
-          User(
-          name: faker.person.name(),
-          email: faker.internet.email()
-          )
-      );
-    }
 
-    users.sort((a, b)=> a.name.toLowerCase().compareTo(b.name.toLowerCase()));
+    _subscription = firestore.collection('users')
+        .snapshots().listen((snapshot){
+      users = snapshot.documents.map((e) => User.fromDocument(e)).toList();
+      users.sort((a, b) =>
+          a.name.toLowerCase().compareTo(b.name.toLowerCase()));
 
-    notifyListeners();
+      notifyListeners();
+    });
+
   }
 
-    List<String> get names => users.map((e)=> e.name).toList();
+  List<String> get names => users.map((e)=> e.name).toList();
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
 }
