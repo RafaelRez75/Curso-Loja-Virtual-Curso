ab1c21c8d000d03630e89ec06a4e08701d25c2b7
RafaelRez75 <rrezende75@gmail.com>
2020-11-28 12:01:15 -0300

    Aula 74, Seção 14 - Adicionando/Removendo um Tamanho

--- lib/screens/edit_product/components/edit_item_size.dart
+++ lib/screens/edit_product/components/edit_item_size.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/common/custom_icon_button.dart';
 import 'package:lojavirtual/models/item_size.dart';
 
 class EditItemSize extends StatelessWidget {
 
-  const EditItemSize({this.size});
+  const EditItemSize({this.size, this.onRemove});
 
   final ItemSize size;
+
+  final VoidCallback onRemove;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
         Expanded(
+          flex: 30,
           child: TextFormField(
             initialValue: size.name,
             decoration: const InputDecoration(hintText: 'Título', labelText: 'Título', isDense: true),
@@ -19,20 +23,35 @@ class EditItemSize extends StatelessWidget {
         ),
         const SizedBox(width: 4,),
         Expanded(
+          flex: 30,
           child: TextFormField(
-            initialValue: size.stock.toString(),
+            initialValue: size.stock?.toString(),
             decoration: const InputDecoration(hintText: 'Estoque', labelText: 'Estoque', isDense: true),
             keyboardType: TextInputType.number,
           ),
         ),
         const SizedBox(width: 4,),
         Expanded(
+          flex: 40,
           child: TextFormField(
-            initialValue: size.price.toStringAsFixed(2),
-            decoration: const InputDecoration(hintText: 'Preço', labelText: 'Preço', isDense: true),
+            initialValue: size.price?.toStringAsFixed(2),
+            decoration: const InputDecoration(hintText: 'Preço', labelText: 'Preço', isDense: true, prefixText: 'R\$'),
             keyboardType: const TextInputType.numberWithOptions(decimal: true),
           ),
-        )
+        ),
+        CustomIconButton(
+          iconData: Icons.remove,
+          color: Colors.red,
+          onTap: onRemove,
+        ),
+        CustomIconButton(
+          iconData: Icons.arrow_drop_up,
+          color: Colors.black,
+        ),
+        CustomIconButton(
+          iconData: Icons.arrow_drop_down,
+          color: Colors.black,
+        ),
       ],
     );
   }
--- lib/screens/edit_product/components/sizes_form.dart
+++ lib/screens/edit_product/components/sizes_form.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/common/custom_icon_button.dart';
 import 'package:lojavirtual/models/item_size.dart';
 import 'package:lojavirtual/models/product.dart';
 import 'package:lojavirtual/screens/edit_product/components/edit_item_size.dart';
 
-class SizesForm extends StatelessWidget {
+  class SizesForm extends StatelessWidget {
   const SizesForm(this.product);
 
   final Product product;
@@ -11,20 +12,45 @@ class SizesForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FormField<List<ItemSize>>(
-      initialValue: product.sizes,
-      builder: (state){
-        return Column(
+    initialValue: List.from(product.sizes),
+    builder: (state){
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Text(
+                'Tamanhos',
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500
+                ),
+              ),
+            ),
+            CustomIconButton(
+              iconData: Icons.add,
+              color: Colors.black,
+              onTap: (){
+                state.value.add(ItemSize());
+                state.didChange(state.value);
+              },
+            )
+          ],
+        ),
+        Column(
           children: state.value.map((size){
             return EditItemSize(
-              size: size
+              size: size,
+              onRemove: (){
+                state.value.remove(size);
+                state.didChange(state.value);
+              },
             );
-
-
-          }).toList()
-
-
-        );
-      },
+          }).toList(),
+        ),
+      ],
     );
-  }
+  },
+  );
+}
 }
