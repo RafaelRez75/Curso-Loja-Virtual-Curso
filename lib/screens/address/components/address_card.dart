2df8e751077c0748cb0eaad65599f48bc0eda041
RafaelRez75 <rrezende75@gmail.com>
2020-12-05 22:42:21 -0300

    Aula 99, Seção 17 - Convertendo o Objeto de Endereço

--- lib/screens/address/components/address_card.dart
+++ lib/screens/address/components/address_card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lojavirtual/models/cart_manager.dart';
 import 'package:lojavirtual/screens/address/components/cep_input_field.dart';
+import 'package:provider/provider.dart';
 
 class AddressCard extends StatelessWidget {
   @override
@@ -9,22 +11,27 @@ class AddressCard extends StatelessWidget {
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: Padding(
         padding: const EdgeInsets.fromLTRB(16, 16, 16, 4),
-        child: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'Endereço de Entrega',
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16
-                ),
+        child: Consumer<CartManager>(
+          builder:(_, cartManager, __){
+            final address = cartManager.address;
+            return Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Endereço de Entrega',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16
+                    ),
+                  ),
+                  CepInputField()
+                ],
               ),
-              CepInputField()
-            ],
-          ),
-        ),
+            );
+          }
+        )
       ),
     );
   }
