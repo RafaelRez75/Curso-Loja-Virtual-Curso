68b028d1870853fd7cafa13afbeb5fabf6fab60f
RafaelRez75 <rrezende75@gmail.com>
2020-12-07 11:47:44 -0300

    Aula 109, Seção 18 - Entendendo a Lógica da Transação

--- lib/models/checkout_manager.dart
+++ lib/models/checkout_manager.dart
@@ -10,4 +10,15 @@ class CheckoutManager extends ChangeNotifier{
     this.cartManager = cartManager;
   }
 
+
+  void checkout() {
+    _decrementStock();
+  }
+
+  void _decrementStock(){
+    // 1. Ler todos os estoques 3xM
+    // 2. Decremento localmente os estoques 2xM
+    // 3. Salvar os estoques no firebase 2xM
+  }
+
 }
--- lib/screens/checkout/checkout_screen.dart
+++ lib/screens/checkout/checkout_screen.dart
@@ -16,16 +16,20 @@ class CheckoutScreen extends StatelessWidget {
           title: const Text('Pagamento'),
           centerTitle: true,
         ),
-        body: ListView(
-          children: <Widget>[
-            PriceCard(
-              buttonText: 'Finalizar Pedido',
-              onPressed: (){
+        body: Consumer<CheckoutManager>(
+          builder: (_, checkoutManager, __){
+            return ListView(
+              children: <Widget>[
+                PriceCard(
+                  buttonText: 'Finalizar Pedido',
+                  onPressed: (){
 
-              },
-            )
-          ],
-        ),
+                  },
+                )
+              ],
+            );
+          }
+        )
       ),
     );
   }
